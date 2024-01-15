29a0cadbfa28b9826df55b4016abb45e5e45024e
AlphaGotReal <patrasubham2004@gmail.com>
2024-01-15 11:55:21 +0530

    neat ai

---
+++ build/CMakeFiles/neat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neat.dir/src/utils.cpp.o"
+  "neat.cpython-38-x86_64-linux-gnu.so"
+  "neat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
